37862129cde12d6d8f9f7288ff4e173085cd19c5
Simon X <simonxiong11@gmail.com>
2020-12-10 20:39:43 -0500

    Mock up login form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-class App extends React.Component {
-  render() {
-    return (
+import React from 'react'
+
+function Form() {
+  return (
     <div>
       <form>
-        <h2>Please Log in</h2>
-        <label> 
-          email: <input type='email' name='email' required={true}/>
-          password: <input type='password' name='password' required={true} />
+        <label>
+         Email 
+          <input type='text' name='user' />
         </label>
-        <button type='submit'>Submit</button>
+        <label>
+          Password
+          <input type='text' name='password' />
+        </label>
+        <input type='submit' name='submit' />
       </form>
     </div>
-    )
-  }
+  )
 }
 
-export default App;
-
+export default Form;
