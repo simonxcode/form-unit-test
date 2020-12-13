6238d2664172e927242b1a70ebc457e79dab1571
Simon X <simonxiong11@gmail.com>
2020-12-13 16:59:46 -0500

    Add dummy links to form

--- src/Form.js
+++ src/Form.js
@@ -9,11 +9,24 @@ function Form() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type='text' placeholder='jondoe@gmail.com' name='email' ref={register} />
-      <input type='password' name='password' ref={register} />
-      <input type='submit' />
-    </form>
+    <div>
+      <h1>
+      Please Sign In
+      </h1>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type='text' placeholder='jondoe@gmail.com' name='email' ref={register} />
+          <div>
+            <input type='password' name='password' ref={register} />
+            <a href='/' >forgot password?</a>
+          </div>
+          <input type='submit' />
+        </form>
+        <div>
+          <a href='/' >New here? Create an account</a>
+        </div>
+      </div>
+    </div>
   );
 }
 
