972c71eec2cc2a895ef9df0733427acaf23f5b31
Simon X <simonxiong11@gmail.com>
2020-12-15 18:04:12 -0500

    Add first and last name input

--- src/Form.js
+++ src/Form.js
@@ -15,6 +15,24 @@ function Form() {
       </h1>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input 
+            type='text'
+            name='firstName'
+            placeholder='first name'
+            ref={register({ required: 'first name required' })}
+          />
+          {errors.firstName && <p>{errors.firstName.message}</p>}
+        </div>
+        <div>
+          <input 
+            type='text'
+            name='lastName'
+            placeholder='last name'
+            ref={register({ required: 'last name required' })}
+          />
+          {errors.lastName && <p>{errors.lastName.message}</p>}
+        </div>
           <input 
             type='text' 
             name='email' 
@@ -22,16 +40,16 @@ function Form() {
             ref={register({ required: 'email required', pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: 'invalid email address'} })} 
             />
           {errors.email && <p>{errors.email.message}</p>}
-          <div>
-            <input 
-              type='password'
-              name='password' 
-              placeholder='password'
-              ref={register({ required: 'password required', pattern: { value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i, message: 'invalid password'} })} 
-              />
-            {errors.password && <p>{errors.password.message}</p>}
-          </div>
-          <input type='submit' />
+        <div>
+          <input 
+            type='password'
+            name='password' 
+            placeholder='password'
+            ref={register({ required: 'password required', pattern: { value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i, message: 'invalid password'} })} 
+            />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
+        <input type='submit' />
         </form>
       </div>
     </div>
