7f862a7419a4c5341d6cd12f18687965df4e29b2
Simon X <simonxiong11@gmail.com>
2020-12-26 15:13:38 -0500

    Install and add react-icon

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,26 +1,12 @@
 import React from 'react'
+import styled from 'styled-component'
+import { IconContext } from 'react-icons'
+import { FiPlus, FiMinus } from 'react-icon/fi'
 
 const Accordion = () => {
   return (
     <div>
-       <p>Rules for Inputs</p>
-        <div>
-          <p>username</p>
-          <ul>
-            <li>must be 5 - 15 characters long</li>
-            <li>must start with a letter</li>
-            <li>only alphanumeric characters allowed</li>
-            <li>only lowercase letters allowed</li>
-          </ul>
-          <p>password</p>
-          <ul>
-            <li>must be 8 - 15 characters long</li>
-            <li>must contain one UPPERCASE letter</li>
-            <li>must contain one lowercase letter</li>
-            <li>must contain one number</li>
-            <li>must contain one special character: @$!%*?&</li>
-          </ul>
-        </div>
+       Accordion
     </div>
   )
 }
