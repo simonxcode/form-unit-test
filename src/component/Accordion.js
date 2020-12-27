c4de1e6e02e944801cce1f88b9125f4e8d75af6a
Simon X <simonxiong11@gmail.com>
2020-12-27 17:02:38 -0500

    Render data from Accordion to Form

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,13 +1,40 @@
 import React from 'react'
-import styled from 'styled-component'
+import { Data } from './Data'
+import styled from 'styled-components'
 import { IconContext } from 'react-icons'
-import { FiPlus, FiMinus } from 'react-icon/fi'
+import { FiPlus, FiMinus } from 'react-icons/fi'
+
+const AccordionSection = styled.div`
+ 
+`
+
+const Container = styled.div`
+
+`
+
+const Wrapper = styled.div`
+
+`
 
 const Accordion = () => {
-  return (
-    <div>
-       Accordion
-    </div>
+  return ( 
+    <IconContext.Provider value = {{ color: `#00805b`, size: '1.3rem' }}>
+      <AccordionSection>
+        <Container>
+          {Data.map((item, index) => {
+            return (
+              <>
+                <Wrapper>
+                  <h3>{item.title}</h3>
+                </Wrapper>
+                  <p>{item.description}</p>
+              </>
+            )
+          })}
+        </Container>
+      </AccordionSection>
+    </IconContext.Provider>
+   
   )
 }
 
