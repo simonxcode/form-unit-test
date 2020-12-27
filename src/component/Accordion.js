c4903099cece6ced9bd8291dfdec21b9ea238248
Simon X <simonxiong11@gmail.com>
2020-12-27 18:08:54 -0500

    Add State to toggle open/close functionality

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Data } from './Data'
 import styled from 'styled-components'
 import { IconContext } from 'react-icons'
@@ -13,7 +13,8 @@ const AccordionSection = styled.div`
 
 const Container = styled.div`
   top: 30%;
-  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3) 
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+  border-radius: .5rem;
 `
 
 const Wrapper = styled.div`
@@ -24,25 +25,56 @@ const Wrapper = styled.div`
   text-align: left;
   cursor: pointer;
 
-  h1 {
-    padding: 2rem;
+  h3 {
+    padding: 1rem;
+    font-size: 1rem;
+  }
+
+  span {
+    margin: 1.5rem;
+  }
+
+  p {
     font-size: 2rem;
   }
 `
 
+const Dropdown = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: 1px solid black;
+  border-top: 1px solid black;
+`
+
 const Accordion = () => {
+  const [clicked, setClicked] = useState(false)
+  const toggle = index => {
+    if(clicked === index){
+      return setClicked(null)
+    }
+    setClicked(index)
+  } 
+
   return ( 
     <IconContext.Provider value = {{ color: `#00805b`, size: '1.3rem' }}>
       <AccordionSection>
         <Container>
           {Data.map((item, index) => {
             return (
-              <>
-                <Wrapper>
+               <div>
+                <Wrapper onClick={() => toggle(index)} key={index}>
                   <h3>{item.title}</h3>
+                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
                 </Wrapper>
-                  <p>{item.description}</p>
-              </>
+                  {clicked === index ? (
+                    <Dropdown>
+                      <p>{item.description}</p>
+                    </Dropdown>
+                  ): null}
+              </div>
             )
           })}
         </Container>
